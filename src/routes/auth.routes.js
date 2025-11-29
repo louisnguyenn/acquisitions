2e548690d9f76ba7333376e034080192f26b35f8
Louis Nguyen <louis.nguyen550@gmail.com>
2025-11-29 13:56:50 -0500

    fix: add req and res to function parameters

--- src/routes/auth.routes.js
+++ src/routes/auth.routes.js
@@ -2,15 +2,15 @@ import express from 'express';
 
 const router = express.Router();
 
-router.post('/sign-up', () => {
+router.post('/sign-up', (req, res) => {
   res.send('POST /api/auth/sign-up response');
 });
 
-router.post('/sign-in', () => {
+router.post('/sign-in', (req, res) => {
   res.send('POST /api/auth/sign-in response');
 });
 
-router.post('/sign-out', () => {
+router.post('/sign-out', (req, res) => {
   res.send('POST /api/auth/sign-out response');
 });
 
