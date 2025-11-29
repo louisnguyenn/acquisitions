d8c38e428279b9e7d395a53642949cebe3dd38b7
Louis Nguyen <louis.nguyen550@gmail.com>
2025-11-29 10:46:09 -0500

    chore(deps): install morgan for logger middleware for tracking activity/traffic

--- src/app.js
+++ src/app.js
@@ -1,8 +1,11 @@
 // setting up express application with middleware
 import logger from '#config/logger.js';
 import express from 'express';
+import helmet from 'helmet';
 
 const app = express();
+
+app.use(helmet());  // middleware for security headers
 
 app.get('/', (req, res) => {
   logger.info('Hello from Acquisitions API');
