329c804c95ae7b03a3331fb51076cec61242d60e
Louis Nguyen <louis.nguyen550@gmail.com>
2025-11-29 10:36:25 -0500

    feat: implement logger info in app

--- src/app.js
+++ src/app.js
@@ -1,10 +1,12 @@
 // setting up express application with middleware
 import express from 'express';
+import logger from "./config/logger.js";
 
 const app = express();
 
 app.get('/', (req, res) => {
-  res.status(200).send('Hello, World!');
+  logger.info('Hello from Acquisitions API');
+  res.status(200).send('Hello, from Acquisitions!');
 });
 
 export default app;
