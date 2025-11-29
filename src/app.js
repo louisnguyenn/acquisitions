dfbf1f0afbd8f33670a48ec222ac28ab3dbc388b
Louis Nguyen <louis.nguyen550@gmail.com>
2025-11-29 10:48:31 -0500

    feat: implement morgan and winston middleware for logging

--- src/app.js
+++ src/app.js
@@ -2,10 +2,19 @@
 import logger from '#config/logger.js';
 import express from 'express';
 import helmet from 'helmet';
+import morgan from 'morgan';
 
 const app = express();
 
-app.use(helmet());  // middleware for security headers
+app.use(helmet()); // middleware for security headers
+app.use(express.json()); // middleware to parse JSON bodies
+app.use(express.urlencoded({ extended: true })); // middleware to parse URL-encoded bodies
+
+// setup morgan to use winston for logging
+// passing morgan logs to winston logger
+app.use(
+  morgan('combined', { stream: { write: (msg) => logger.info(msg.trim()) } })
+); // logging middleware
 
 app.get('/', (req, res) => {
   logger.info('Hello from Acquisitions API');
