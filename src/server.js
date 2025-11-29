0e3704a4fe12bfeae375f6203a82c5389ebeddb2
Louis Nguyen <louis.nguyen550@gmail.com>
2025-11-29 09:56:16 -0500

    feat(server): change console log message

--- src/server.js
+++ src/server.js
@@ -1,8 +1,8 @@
 // running express server
-import app from './app';
+import app from './app.js';
 
 const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
-  console.log(`Listening on ${PORT}...`);
+  console.log(`Listening on http://localhost:${PORT}`);
 });
