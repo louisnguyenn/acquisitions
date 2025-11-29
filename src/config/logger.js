49be8c60114e563367c916ba9f17f52ff71394bb
Louis Nguyen <louis.nguyen550@gmail.com>
2025-11-29 10:54:08 -0500

    style: create log in correct logging folder

--- src/config/logger.js
+++ src/config/logger.js
@@ -10,7 +10,7 @@ const logger = winston.createLogger({
   defaultMeta: { service: 'acquisitions-api' },
   transports: [
     new winston.transports.File({ filename: 'logs/error.log', level: 'error' }),
-    new winston.transports.File({ filename: 'combined.log' }),
+    new winston.transports.File({ filename: 'logs/combined.log' }),
   ],
 });
 
